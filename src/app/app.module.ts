513e230e09672cee27578a0e9de7edcb0511c62f
Rahul Nayak <nayakrahul2015@gmail.com>
2024-07-01 21:04:01 +0530

    first commit for engispider home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+
+
+  // Card scroll animation start
+  customOptionsDisplayImage: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000, // Set the time between slides in milliseconds
+    autoplayHoverPause: true, // Pause autoplay when hovering over the carousel
+    items: 1,
+    navSpeed: 900,
+    navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      710: {
+        items: 1
+      },
+      940: {
+        items: 1
+      },
+      1400: {
+        items: 1
+      }
+    },
+    nav: false
+  }
+
+}
